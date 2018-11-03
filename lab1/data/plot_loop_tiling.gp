6a3eeb19a5cdb5c1209959ef7d9a91cde1933137
Davide Ponzini <davide.ponzini95@gmail.com>
2018-11-03 10:07:30 +0100

    lab1 data

--- lab1/data/plot_loop_tiling.gp
+++ lab1/data/plot_loop_tiling.gp
@@ -5,7 +5,7 @@ set style data lines
 set xlabel "matrix size" offset -1,0
 set xrange [10 : 6100]
 set ylabel "performance (GFLOPS)" offset 0,-1
-set yrange [0 : 3]
+set yrange [0 : 4]
 
 plot \
 "loop_tiling_intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O0.dat" t "Intel Core i5 650 3.2GHz, 256KB L2 cache, DDR3 662 MHz, gcc 7.3.0 -O0",\
--- lab1/script.sh
+++ lab1/script.sh
@@ -1,6 +1,6 @@
 for i in 0 1 2 3 ; do
 	g++ -std=c++11 -O$i -o mm_$i ${prefix}matmul.cpp || exit;
-	./mm_$i 16 1000 4 > ${prefix}intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O$i.dat
+#	./mm_$i 16 1000 4 > ${prefix}intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O$i.dat
 #	./mm_$i 1100 3000 100 >> ${prefix}intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O$i.dat
-#	./mm_$i 4000 6000 1000 >> ${prefix}intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O$i.dat
+	./mm_$i 4000 6000 1000 >> ${prefix}intel-corei5-650-3.2GHz_DDR3-662MHz_gcc730-O$i.dat
 done
