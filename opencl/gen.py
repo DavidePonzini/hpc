682440a3f62245409128515d8ef366bcb9442f95
Davide Ponzini <davide.ponzini95@gmail.com>
2018-12-03 14:43:47 +0100

    gen %

--- opencl/gen.py
+++ opencl/gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from random import randrange
-from sys import argv
+from sys import argv, stderr
 
 if len(argv) < 5:
 	print("i, j, minv, maxv, p")
@@ -14,6 +14,8 @@ minv = int(argv[3])
 maxv = int(argv[4])+1
 p = int(argv[4])
 
+tot = i*j
+
 for ii in range(i):
 	for jj in range(j):
 		if randrange(p) == 0:
@@ -21,3 +23,4 @@ for ii in range(i):
 		else:
 			val = 0
 		print('{} {} {}'.format(ii, jj, val))
+		print('{:.3f}%\r'.format((ii*j+jj)/tot*100), flush=True, file=stderr, end='')
