cad95bf4167f035ff6cdbb585c998e0eaad634a4
Davide Ponzini <davide.ponzini95@gmail.com>
2018-12-04 12:44:21 +0100

    faster gen (less random)

--- opencl/gen.py
+++ opencl/gen.py
@@ -3,24 +3,28 @@
 from random import randrange
 from sys import argv, stderr
 
-if len(argv) < 5:
-	print("i, j, minv, maxv, p")
+if len(argv) < 9:
+	print("i, j, min_i, max_i, min_j, max_j, minv, maxv")
 	exit(1)
 
 
 i = int(argv[1])
 j = int(argv[2])
-minv = int(argv[3])
-maxv = int(argv[4])+1
-p = int(argv[4])
+min_i = int(argv[3])
+max_i = int(argv[4])
+min_j = int(argv[5])
+max_j = int(argv[6])
+min_v = int(argv[7])
+max_v = int(argv[8])+1
 
 tot = i*j
 
 for ii in range(i):
 	for jj in range(j):
-		if randrange(p) == 0:
-			val = minv+randrange(maxv-minv)
-		else:
+		if ii < min_i or ii > max_i or jj < min_j or jj > max_j:
 			val = 0
+		else:
+			val = min_v+randrange(max_v-min_v)
+
 		print('{} {} {}'.format(ii, jj, val))
 		print('{:.3f}%\r'.format((ii*j+jj)/tot*100), flush=True, file=stderr, end='')
